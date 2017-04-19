dd8044ae335bdb7395f890ba78b77f116e3a9863
Ziserman <martinziserman@gmail.com>
2017-04-19 20:31:05 +0200

    navigation

--- crawler.js
+++ crawler.js
@@ -3,27 +3,45 @@ var cheerio = require('cheerio');
 var URL = require('url-parse');
 
 
-var baseUrl = "http://www.transparency.org/whatwedo/publications";
-var pagesToVisit = [baseUrl]
+var baseUrl = "http://www.transparency.org";
+var pagesToVisit = [`${baseUrl}/whatwedo/publications`]
+var articles = [];
+
 for (var i = 0; i < 35; i++) {
-  pagesToVisit.push(`${baseUrl}/P${(i+1)*10}`)
+  // pagesToVisit.push(`${baseUrl}/whatwedo/publications/P${(i+1)*10}`)
 }
 // console.log(pagesToVisit)
 
 pagesToVisit.map((page) => {
-  console.log("Visiting page " + page);
+  // console.log("Visiting page " + page);
   request(page, function(error, response, body) {
     if (error) {
       console.log("Error: " + error);
     }
     // Check status code (200 is HTTP OK)
-    console.log("Status code: " + response.statusCode);
+    // console.log("Status code: " + response.statusCode);
     if (response.statusCode === 200) {
       // Parse the document body
       var $ = cheerio.load(body);
-      var articles = $('#Publications .article.story').map((i, article) => {
-        console.log($(this).find("h3 a").text())
+      $('#Publications .article.story').map((i, article) => {
+        articles.push({
+          "name": $(article).find("h3 a").text(),
+          "link": $(article).find("h3 a").attr("href")
+        });
       })
+      if (page == pagesToVisit[pagesToVisit.length - 1]) {
+        crawlArticles();
+        console.log(articles);
+      }
     }
   })
-})
+});
+
+function crawlArticles() {
+  var articlesToVisit = articles.map(x => baseUrl + x.link);
+  console.log(articlesToVisit);
+
+}
+
+// sleep(10);
+console.log(articles)
